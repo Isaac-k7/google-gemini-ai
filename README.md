8e7fd42406ec6be7946beb7f447aef294a23ea49
Isaac <isaac@bigfiveabidjan.com>
2023-12-19 22:40:30 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# gemini_ai_test
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+
+## Gemini AI Test
+
+Gemini AI Test is a simple chat app that shows how to use the Gemini API.
+
+![App Screenshot](assets/img/screenshort/i1.png)
+
---
+++ lib/home.dart
@@ -0,0 +1,248 @@
+import 'package:flutter/material.dart';
+import 'package:google_gemini/google_gemini.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import 'package:chat_bubbles/chat_bubbles.dart';
+
+const apiKey = 'AIzaSyAebuVd7d_d0VRy4m8OPBHYW4TbaNviADE';
+
+class Home extends StatefulWidget {
+  const Home({
+    super.key,
+  });
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Google Gemini"),
+        centerTitle: true,
+      ),
+      body: const TextWithImage(),
+    );
+  }
+}
+
+// ------------------------------ Text with Image ------------------------------
+
+class TextWithImage extends StatefulWidget {
+  const TextWithImage({
+    super.key,
+  });
+
+  @override
+  State<TextWithImage> createState() => _TextWithImageState();
+}
+
+class _TextWithImageState extends State<TextWithImage> {
+  bool loading = false;
+  List textAndImageChat = [];
+  List textWithImageChat = [];
+  File? imageFile;
+
+  final ImagePicker picker = ImagePicker();
+
+  final TextEditingController _textController = TextEditingController();
+  final ScrollController _controller = ScrollController();
+
+  // Create Gemini Instance
+  final gemini = GoogleGemini(
+    apiKey: apiKey,
+  );
+
+  // Text only input
+  void fromTextAndImage({required String query, required File image}) {
+    setState(() {
+      loading = true;
+      textAndImageChat.add({
+        "role": "User",
+        "text": query,
+        "image": image,
+      });
+      _textController.clear();
+      imageFile = null;
+    });
+    scrollToTheEnd();
+
+    gemini.generateFromTextAndImages(query: query, image: image).then((value) {
+      setState(() {
+        loading = false;
+        textAndImageChat
+            .add({"role": "Gemini", "text": value.text, "image": ""});
+      });
+      scrollToTheEnd();
+    }).onError((error, stackTrace) {
+      setState(() {
+        loading = false;
+        textAndImageChat
+            .add({"role": "Gemini", "text": error.toString(), "image": ""});
+      });
+      scrollToTheEnd();
+    });
+  }
+
+  // Text only input
+  void fromText({required String query}) {
+    setState(() {
+      loading = true;
+      textAndImageChat.add({
+        "role": "User",
+        "text": query,
+      });
+      _textController.clear();
+    });
+    scrollToTheEnd();
+
+    gemini.generateFromText(query).then((value) {
+      setState(() {
+        loading = false;
+        textAndImageChat.add({
+          "role": "Gemini",
+          "text": value.text,
+        });
+      });
+      scrollToTheEnd();
+    }).onError((error, stackTrace) {
+      setState(() {
+        loading = false;
+        textAndImageChat.add({
+          "role": "Gemini",
+          "text": error.toString(),
+        });
+      });
+      scrollToTheEnd();
+    });
+  }
+
+  void scrollToTheEnd() {
+    _controller.jumpTo(_controller.position.maxScrollExtent);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              controller: _controller,
+              itemCount: textAndImageChat.length,
+              padding: const EdgeInsets.only(bottom: 20),
+              itemBuilder: (context, index) {
+                return Column(
+                  children: [
+                    if (textAndImageChat[index]["image"] != '' &&
+                        textAndImageChat[index]["image"] != null)
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child:
+                            // AspectRatio(
+                            //   aspectRatio: 16 / 9,
+                            // child:
+                            Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            SizedBox(
+                              width: MediaQuery.sizeOf(context).width * .7,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: Image.file(
+                                    textAndImageChat[index]["image"]),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    // ),
+                    BubbleSpecialThree(
+                      text: textAndImageChat[index]["text"],
+                      color: textAndImageChat[index]["role"] == 'User'
+                          ? Theme.of(context).colorScheme.primary
+                          : Theme.of(context).colorScheme.secondary,
+                      tail: true,
+                      textStyle: TextStyle(
+                          color: textAndImageChat[index]["role"] == 'User'
+                              ? Theme.of(context).colorScheme.onPrimary
+                              : Theme.of(context).colorScheme.onSecondary,
+                          fontSize: 16),
+                      isSender: textAndImageChat[index]["role"] == 'User',
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+          Container(
+            alignment: Alignment.bottomRight,
+            margin: const EdgeInsets.all(20),
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              border:
+                  Border.all(color: Theme.of(context).colorScheme.secondary),
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _textController,
+                    decoration: InputDecoration(
+                      hintText: "Write a message",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: BorderSide.none),
+                      fillColor: Theme.of(context).colorScheme.secondary,
+                    ),
+                    maxLines: null,
+                    keyboardType: TextInputType.multiline,
+                  ),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.add_a_photo),
+                  onPressed: () async {
+                    final XFile? image =
+                        await picker.pickImage(source: ImageSource.gallery);
+                    setState(() {
+                      imageFile = image != null ? File(image.path) : null;
+                    });
+                  },
+                ),
+                IconButton(
+                  icon: loading
+                      ? const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: CircularProgressIndicator(strokeWidth: 2),
+                      )
+                      : Icon(
+                          Icons.send,
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                  onPressed: () {
+                    if (imageFile == null) {
+                      fromText(query: _textController.text);
+                      return;
+                    }
+                    fromTextAndImage(
+                        query: _textController.text, image: imageFile!);
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: imageFile != null
+          ? Container(
+              margin: const EdgeInsets.only(bottom: 80),
+              height: 150,
+              child: Image.file(imageFile ?? File("")),
+            )
+          : null,
+    );
+  }
+}
